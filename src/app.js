cfe1f6714a0fd0eb0d20af548935b2cf79831690
Irene VS <147383929+iireneVS@users.noreply.github.com>
2023-12-14 12:02:39 +0000

    random finalizado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Datos individuales de los dominios.
+
+  let articles = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let tlds = [".com", ".net"];
+
+  let domainNames = [];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tld of tlds) {
+          domainNames.push(`${article}${adjective}${noun}${tld}`);
+        }
+      }
+    }
+  }
+
+  document.querySelector("#generate").onclick = () => {
+    document.querySelector("#domain").innerHTML = `<h1>${
+      domainNames[Math.floor(Math.random() * domainNames.length)]
+    }</h1>`;
+  };
 };
